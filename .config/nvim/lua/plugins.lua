c702c81853b42d46b35293dd2c840326f9ef8458
Oscar te Giffel <oscar@tegiffel.com>
2022-06-01 15:27:19 +0200

    feat: remove lspkind

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -41,7 +41,6 @@ return require('packer').startup(function(use)
   use "hrsh7th/cmp-path"
   use "hrsh7th/cmp-cmdline"
   use "hrsh7th/nvim-cmp"
-  use "onsails/lspkind.nvim"
   use 'L3MON4D3/LuaSnip'
   use 'saadparwaiz1/cmp_luasnip'
   use "rafamadriz/friendly-snippets"
