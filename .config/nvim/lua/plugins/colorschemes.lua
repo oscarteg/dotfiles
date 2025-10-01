4fa99ba3a955b5bf5dd03cc959bcdb125ebc2a2d
Oscar te Giffel <oscar@tegiffel.com>
2025-10-01 22:55:33 +0200

    feat: yugen as colorschem

--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -105,7 +105,19 @@ return {
       },
     },
   },
-
+  {
+    "f-person/auto-dark-mode.nvim",
+    opts = {
+      set_dark_mode = function()
+        vim.api.nvim_set_option_value("background", "dark", {})
+      end,
+      set_light_mode = function()
+        vim.api.nvim_set_option_value("background", "light", {})
+      end,
+      update_interval = 3000,
+      fallback = "dark",
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = {
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -14,19 +14,6 @@ return {
     },
   },
   {
-    "f-person/auto-dark-mode.nvim",
-    opts = {
-      set_dark_mode = function()
-        vim.api.nvim_set_option_value("background", "dark", {})
-      end,
-      set_light_mode = function()
-        vim.api.nvim_set_option_value("background", "light", {})
-      end,
-      update_interval = 3000,
-      fallback = "dark",
-    },
-  },
-  {
     "nvim-lualine/lualine.nvim",
     opts = {
       theme = "rose-pine-main",
